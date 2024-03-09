55936239803dcac3c97d0cf3d55ef6c01a3bc00c
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-03-09 15:01:23 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import Categories from './components/Categories'
+import Campaigns from './components/Campaigns'
+import Favorites from './components/Favorites'
+import MobileApp from './components/MobileApp'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 
-const App = () => {
+export default function App() {
   return (
-    <div className='bg-red-500'>
-      og
-    </div>
+    <>
+      <Header />
+      <HeroSection />
+      <Categories />
+      <Campaigns />
+      <Favorites />
+      <MobileApp />
+      <Cards />
+      <Footer />
+    </>
   )
 }
-
-export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        "brand-color": "#4c3398",
+      }),
+    },
   },
   plugins: [],
 };
