e1657f4dc032dac1d4613835319d4ca0c40a3200
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-03-11 06:19:09 +0300

    mobile-app

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       spacing: {
         0.1: "0.063rem",
       },
+      backgroundImage: (theme) => ({
+        "mobile-app":
+          "url(https://cdn.getir.com/getirweb-images/common/illustration/doodle.png)",
+      }),
       backgroundColor: (theme) => ({
         "brand-color": "#4c3398",
         "primary-brand-color": "#5d3ebc",
