39eb09991d75d6ec2fe61c155cc2506c3394ce40
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-03-11 11:46:33 +0300

    footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,21 +12,21 @@ function Footer() {
 						</h3>
 						<ul className="grid grid-cols-2 sm:grid-cols-1 gap-y-3">
 							<li>
-								<a href="#">
+								<a href="https://apps.apple.com/us/app/getir-groceries-in-minutes/id995280265">
 									<img
 										src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg"
 										alt=""/>
 								</a>
 							</li>
 							<li>
-								<a href="#">
+								<a href="https://play.google.com/store/apps/details?id=com.getir">
 									<img
 										src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg"
 										alt=""/>
 								</a>
 							</li>
 							<li>
-								<a href="#">
+								<a href="https://appgallery.huawei.com/#/app/C100954039">
 									<img
 										src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg"
 										alt=""/>
